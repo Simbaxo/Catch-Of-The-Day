8d0432f84777b9f3d47b960600339e536e58e636
Andres <ballerlikemike23@gmail.com>
2017-11-10 22:16:37 -0500

    Persisting our State with Firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDPITOZwhLn-JgjQ9UsCXG7Kvs8YSR_5cU",
+  authDomain: "catch-of-the-day-simba.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-simba.firebaseio.com"
+});
+
+export default base;
